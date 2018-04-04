138eaf52d374dd4a44dee9653960b3a411b01261
nvidya.karthik <nvidya.karthik@gmail.com>
2018-04-04 12:31:48 -0700

    added code for get movie year from the api

--- liri.js
+++ liri.js
@@ -1,5 +1,24 @@
 require("dotenv").config();
-var keys = require("./keys.js");
+//var keys = require("./keys.js");
+var request = require("request");
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+
+//var spotify = new Spotify(keys.spotify);
+//var client = new Twitter(keys.twitter);
+//console.log(process.env.TWITTER_CONSUMER_KEY)
+var userChoice = process.argv[2];
+if (userChoice === "movie-this") {
+    var movieName = process.argv[3];
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    // This line is just to help us debug against the actual URL.
+    console.log(queryUrl);
+    // Then create a request to the queryUrl
+    request(queryUrl, function (error, response, body) {
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+            // Then log the Release Year for the movie
+            console.log(JSON.parse(body).Year);
+        }
+    });
+
+}
